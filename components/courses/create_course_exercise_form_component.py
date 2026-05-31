b3093f7ba0317b83374949e6e1810b612f787ab4
Viajero <viajero95@mail.ru>
2026-06-01 03:59:48 +0700

    Добавление Allure шагов в автотесты

--- components/courses/create_course_exercise_form_component.py
+++ components/courses/create_course_exercise_form_component.py
@@ -1,8 +1,11 @@
+import allure
+from playwright.sync_api import Page
+
 from components.base_component import BaseComponent
-from playwright.sync_api import expect, Page
 from elements.button import Button
-from elements.text import Text
 from elements.input import Input
+from elements.text import Text
+
 
 class CreateCourseExerciseFormComponent(BaseComponent):
     def __init__(self, page: Page):
@@ -25,6 +28,7 @@ class CreateCourseExerciseFormComponent(BaseComponent):
     def click_delete_button(self, index: int):
         self.delete_exercise_button.click(index=index)
 
+    @allure.step("Check visible create course exercise form at '{index}'")
     def check_visible(self, index: int, title: str, description: str):
         self.subtitle.check_visible(index=index)
         self.subtitle.check_have_text(f"#{index + 1} Exercise", index=index)
@@ -35,12 +39,10 @@ class CreateCourseExerciseFormComponent(BaseComponent):
         self.description_input.check_visible(index=index)
         self.description_input.check_have_value(description, index=index)
 
-
-
+    @allure.step("Fill create course exercise form at '{index}'")
     def fill_create_exercise_form(self, index: int, title: str, description: str):
         self.title_input.fill(title, index=index)
         self.title_input.check_have_value(title, index=index)
 
         self.description_input.fill(description, index=index)
         self.description_input.check_have_value(description, index=index)
-
