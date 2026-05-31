f3482b31fe79fe12659078c40efd73d12f119cbd
Viajero <viajero95@mail.ru>
2026-06-01 02:26:46 +0700

    Интеграция Allure в автотесты

--- tests/authentication/test_registration.py
+++ tests/authentication/test_registration.py
@@ -1,12 +1,24 @@
+import allure
 import pytest
+from allure_commons.types import Severity
 
-from pages.dashboard.dashboard_page import DashboardPage
 from pages.authentication.registration_page import RegistrationPage
+from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStories
+from tools.allure.tags import AllureTag
 
 
 @pytest.mark.regression
 @pytest.mark.registration
+@allure.tag(AllureTag.REGISTRATION, AllureTag.REGRESSION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.story(AllureStories.REGISTRATION)
 class TestRegistration:
+    @allure.title("Registration with correct email, username and password")
+    @allure.severity(Severity.CRITICAL)
     def test_successful_registration(self, registration_page: RegistrationPage, dashboard_page: DashboardPage):
         registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
         registration_page.registration_form.fill(email='user@gmail.com', username='username', password='password')
